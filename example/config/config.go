87b24e0095d32b97b167209a0054cd3f5b1853fa
Kalveir <71752646+Kalveir@users.noreply.github.com>
2025-06-26 06:38:57 +0700

    Update config.go

--- example/config/config.go
+++ example/config/config.go
@@ -11,7 +11,7 @@ import (
 )
 
 // GracefulShutdown handles server shutdown gracefully
-func GracefulShutdown(server *http.Server) {
+func GracefulShutdown(srv *http.Server) {
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
@@ -19,9 +19,10 @@ func GracefulShutdown(server *http.Server) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	if err := server.Shutdown(ctx); err != nil {
-		log.Fatalf("Server forced to shutdown: %v", err)
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Shutdown error: %v", err)
 	}
+	log.Println("Server stopped gracefully")
 }
 
 // LogRequest logs HTTP requests
