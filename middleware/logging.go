3187d1501de1d587510e6af8316cdc6d45018d1e
Kalveir <71752646+Kalveir@users.noreply.github.com>
2025-07-03 12:01:20 +0700

    Update logging.go

--- middleware/logging.go
+++ middleware/logging.go
@@ -3,19 +3,28 @@ package middleware
 import (
 	"log"
 	"net/http"
+	"path"
 	"time"
 )
+
+type Middleware func(http.Handler) http.Handler
 
 func Logging() Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			start := time.Now()
 			defer func() {
+				clientIP := r.RemoteAddr
+				userAgent := r.UserAgent()
+				endpoint := path.Base(r.RequestURI)
+
 				log.Printf(
-					"%s %s %s %v",
+					"%s %s %s %v | Client IP: %s | User-Agent: %s",
 					r.Method,
-					r.RequestURI,
+					endpoint,
 					time.Since(start),
+					clientIP,
+					userAgent,
 				)
 			}()
 			next.ServeHTTP(w, r)
